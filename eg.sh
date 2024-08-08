7fe77ad49cdaf6a5e0f9cdc094fcdeffdf17c3d9
Madhu <vellaturu28@gmail.com>
2024-08-08 09:57:08 -0700

    first batch of bash file added

---
+++ eg.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+echo
+echo "##### file structure #####"
+ls -l
+echo
+
+echo "#### present working directory#####"
+pwd
+echo
+
+echo "#### disk space#####"
+df -h
+echo
+
+echo "#### displat date#####"
+date
+echo
+
+echo "#### hidden files #####"
+ls -a
+echo
