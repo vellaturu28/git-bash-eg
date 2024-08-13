5def9cd7fac509b185a4c9990947b4cf86776bd0
madhu <vellaturu28@gmail.com>
2024-08-12 23:35:10 -0700

    changes added
    committed files

--- eg.sh
+++ eg.sh
@@ -1,3 +1,8 @@
+
+
+
+
+
 #!/bin/bash
 
 echo
--- eg1.sh
+++ eg1.sh
@@ -1,7 +1,16 @@
-#!bin/bash
+
+
+#!/bin/bash
 
 echo
-echo"##### free space ####"
-free -m 
+echo"##### disk space ####"
+df -h 
 echo
+
+echo "enter 2 numbers"
+read $1
+read $2
+echo "$1" + "$2"
+
+
 
